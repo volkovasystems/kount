1d978fcd1584c28fc60bcadefd0572a72d6125d8
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-05 18:36:05 +0800

    Updated test.

--- kount-test.js
+++ kount-test.js
@@ -1,7 +1,10 @@
-"use strict";
-
+const assert = require( "assert" );
 const kount = require( "./kount.js" );
 
-console.log( kount( { "name": "simple" } ) );
-console.log( kount( { } ) );
-console.log( kount( ) );
+assert.equal( kount( { "name": "simple" } ), 1, "should have value 1" );
+
+assert.equal( kount( { } ), 0, "should have value 0" );
+
+assert.equal( kount( ), 0, "should have value 0" );
+
+console.log( "ok" );
